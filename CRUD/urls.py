21aa69715cfac9d92d72a924c17177921e00260e
nammi narasimhamurthy <narasimhamurthynammi712@gmail.com>
2025-05-26 19:28:42 +0530

    hosting on versal.com

---
+++ CRUD/urls.py
@@ -0,0 +1,18 @@
+"""CRUD URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls), 
+    path("", include('crudoperations.urls')),
+]
+
+# Serve media and static files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
